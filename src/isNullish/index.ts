393d0bd79bc77feeb892b3db84676020736543b5
Camille Spain <camille@camillespain.com>
2024-09-19 15:25:42 -0700

    rename isNullish param for consistency and style

--- src/isNullish/index.ts
+++ src/isNullish/index.ts
@@ -1,10 +1,10 @@
 /**
  * Checks if the argument is null or undefined.
- * @param val The value to evaluate.
- * @returns True if val is null or undefined, false otherwise.
+ * @param value The value to evaluate.
+ * @returns True if value is null or undefined, false otherwise.
  */
-export const isNullish = (val: unknown): boolean => {
-  if (val === undefined || val === null) {
+export const isNullish = (value: unknown): boolean => {
+  if (value === undefined || value === null) {
     return true;
   }
   return false;
