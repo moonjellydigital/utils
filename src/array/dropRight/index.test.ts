25784ec0aa2681f374800e59c93e0e0189c05ee3
Camille Spain <camille@camillespain.com>
2024-11-12 12:22:14 -0800

    dropRight

---
+++ src/array/dropRight/index.test.ts
@@ -0,0 +1,74 @@
+import { dropRight } from '.';
+import { describe, test, expect } from 'vitest';
+import type { ErrData } from '../../types';
+
+describe('dropRight', () => {
+  test('should return a TypeError if arr is not array-like', () => {
+    const result = dropRight(
+      new Set() as unknown as ArrayLike<unknown>,
+      2,
+    ) as Error;
+    const errData = result.cause as ErrData;
+    expect(result).toBeInstanceOf(TypeError);
+    expect(errData.code).toBe('WrongType');
+    expect(errData.prevErr).toBe(null);
+    expect(errData.args).toEqual([new Set(), 2]);
+  });
+
+  test('should return a TypeError if n is not of number type', () => {
+    const result = dropRight([1, 2, 3], {
+      count: 4,
+    } as unknown as number) as Error;
+    const errData = result.cause as ErrData;
+    expect(result).toBeInstanceOf(TypeError);
+    expect(errData.code).toBe('WrongType');
+    expect(errData.prevErr).toBe(null);
+    expect(errData.args).toEqual([[1, 2, 3], { count: 4 }]);
+  });
+
+  test.each([
+    [[1, 2, 3], NaN, 'expected placeholder'],
+    [[1, 2, 3], Number.MAX_VALUE, 'expected placeholder'],
+    [[1, 2, 3], -4, 'expected placeholder'],
+  ])(
+    'should return a RangeError if n is NaN, negative, or not a safe integer',
+    (arg1, arg2) => {
+      const result = dropRight(arg1, arg2) as Error;
+      const errData = result.cause as ErrData;
+      expect(result).toBeInstanceOf(RangeError);
+      expect(errData.code).toBe('InvalidRange');
+      expect(errData.prevErr).toBe(null);
+      expect(errData.args).toEqual([arg1, arg2]);
+    },
+  );
+
+  test('should return an empty array if n is greater than arr.length', () => {
+    const result = dropRight([1, 2, 3], 7);
+    expect(result).toStrictEqual([]);
+  });
+
+  test('should not mutate the original array', () => {
+    const arg = [{ name: 'Cindy' }, { name: 'Bob' }];
+    const argCopy = structuredClone(arg);
+    dropRight(arg, 2);
+    expect(arg).toStrictEqual(argCopy);
+  });
+
+  test('should make a shallow copy of objects inside arr', () => {
+    const arg = [{ name: 'Cindy' }, { name: 'Bob' }];
+    const result = dropRight(arg, 0) as object[];
+    expect(result[0]).toBe(arg[0]);
+    expect(result[1]).toBe(arg[1]);
+    expect(result).not.toBe(arg);
+  });
+
+  test.each([
+    [{ 0: 'a', 1: 'b', 3: 'c', length: 4 }, 1, ['a', 'b', ,]], // eslint-disable-line no-sparse-arrays
+    [[1, , 2, 3, 5], new Number(4), [1]], // eslint-disable-line no-sparse-arrays
+    ['string', 2, ['s', 't', 'r', 'i']],
+  ])('dropRight(%s, %s) should equal %s', (arg1, arg2, expected) => {
+    expect(dropRight(arg1 as ArrayLike<unknown>, arg2 as number)).toStrictEqual(
+      expected,
+    );
+  });
+});
